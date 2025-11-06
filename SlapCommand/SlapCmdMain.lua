a3af39b8f7ac49a3ab167e8f3a8a42dd3e070afc
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-11-06 19:13:48 +0100

    Update SlapCmdMain.lua

--- SlapCommand/SlapCmdMain.lua
+++ SlapCommand/SlapCmdMain.lua
@@ -8,7 +8,7 @@
 --5. https://www.roblox.com/games/110876351628508/Wallhop-Slap-Tower
 --6. https://www.roblox.com/games/91711653427804/Slap-Tower-7 (but theres like a 5 sceond cooldown for each player to get slap)
 --7. https://www.roblox.com/games/104002488192102/Omega-Troll-Slap-Tower
-print("version test v 5.0")
+print("version test v 6.0")
 local slap = nil
 local selectingPlayer = false
 local mouseConnection = nil
@@ -505,7 +505,7 @@ if slap ~= nil then
 					end)
 				end
 			end
-			task.wait(3)
+			task.wait(10)
 			stopOrbitCamera()
 		end)
 	end)
