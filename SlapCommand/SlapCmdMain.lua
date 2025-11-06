f4428d077dec8fb34af94889d62b57de6118883a
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-11-06 20:35:30 +0100

    Update SlapCmdMain.lua

--- SlapCommand/SlapCmdMain.lua
+++ SlapCommand/SlapCmdMain.lua
@@ -8,7 +8,7 @@
 --5. https://www.roblox.com/games/110876351628508/Wallhop-Slap-Tower
 --6. https://www.roblox.com/games/91711653427804/Slap-Tower-7 (but theres like a 5 sceond cooldown for each player to get slap)
 --7. https://www.roblox.com/games/104002488192102/Omega-Troll-Slap-Tower
-print("version test v 9.0")
+print("version test v 9.1")
 local slap = nil
 local selectingPlayer = false
 local mouseConnection = nil
@@ -528,6 +528,7 @@ if slap ~= nil then
 								task.spawn(function()
 									slap.Event:FireServer(unpack(args))
 								end)
+								task.wait(0.1)
 							until player.Character:FindFirstChild("HumanoidRootPart").Position.Y >= 300
 							AddLog("Loop (" .. i .. "/10) comepleted!")
 						end
@@ -535,7 +536,7 @@ if slap ~= nil then
 					end)
 				end
 			end
-			task.wait(40)
+			task.wait(25)
 			stopOrbitCamera()
 		end)
 	end)
