a3d62ab64b42a30d09520b6beefa2908bbd8b167
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-11-06 10:35:12 +0100

    Create SlapCmdMain.lua

---
+++ SlapCommand/SlapCmdMain.lua
@@ -0,0 +1,175 @@
+--this script works in all games with a slap that has a remotevent called "Event"
+local slap = nil
+local selectingPlayer = false
+local mouseConnection = nil
+local flingSelectMode = false
+local highlightInstance = nil
+
+function findslap()
+	for i,v in workspace[game.Players.LocalPlayer.Name]:GetDescendants() do
+		if v.Name == "Event" then
+			local slaptool = v.Parent
+			print(slaptool.Name)
+			return slaptool
+		end
+	end
+end
+
+function hit(plr)
+	local args = {
+		"slash",
+		game:GetService("Players"):WaitForChild(plr.Name).Character,
+		Vector3.new(10,10,10)
+	}
+	task.spawn(function()
+		slap.Event:FireServer(unpack(args))
+	end)
+end
+
+function killslap(plr)
+	local args = {
+		"slash",
+		game:GetService("Players"):WaitForChild(plr.Name).Character,
+		Vector3.new(10000000,10000000,1000000)
+	}
+	task.spawn(function()
+		slap.Event:FireServer(unpack(args))
+	end)
+end
+
+function highlightPlayer(plr)
+	-- Remove previous highlight
+	if highlightInstance then
+		highlightInstance:Destroy()
+		highlightInstance = nil
+	end
+	if plr and plr.Character then
+		local Highlight = Instance.new("Highlight")
+		Highlight.Adornee = plr.Character
+		Highlight.FillColor = Color3.fromRGB(255, 255, 0)
+		Highlight.OutlineColor = Color3.fromRGB(255, 0, 0)
+		Highlight.Parent = plr.Character
+		highlightInstance = Highlight
+	end
+end
+
+function removeHighlight()
+	if highlightInstance then
+		highlightInstance:Destroy()
+		highlightInstance = nil
+	end
+end
+
+function enablePlayerSelection()
+	print("active")
+	selectingPlayer = true
+	local player = game.Players.LocalPlayer
+	local mouse = player:GetMouse()
+	mouse.Icon = "rbxassetid://6035047400" -- Optional: change mouse icon for selection mode
+
+	mouseConnection = mouse.Button1Down:Connect(function()
+		if not selectingPlayer then return end
+		local target = mouse.Target
+		if target then
+			local targetParent = target.Parent
+			for _, plr in game.Players:GetPlayers() do
+				if plr ~= player and plr.Character and (plr.Character == targetParent or target:IsDescendantOf(plr.Character)) then
+					hit(plr)
+					selectingPlayer = false
+					mouse.Icon = "" -- Reset mouse icon
+					if mouseConnection then
+						mouseConnection:Disconnect()
+						mouseConnection = nil
+					end
+					break
+				end
+			end
+		end
+	end)
+end
+
+function enableFlingSelectMode()
+	if mouseConnection then
+		mouseConnection:Disconnect()
+		mouseConnection = nil
+	end
+	local player = game.Players.LocalPlayer
+	local mouse = player:GetMouse()
+	mouse.Icon = "rbxassetid://6035047400"
+	mouseConnection = mouse.Button1Down:Connect(function()
+		if not flingSelectMode then return end
+		local target = mouse.Target
+		if target then
+			local targetParent = target.Parent
+			for _, plr in game.Players:GetPlayers() do
+				if plr ~= player and plr.Character and (plr.Character == targetParent or target:IsDescendantOf(plr.Character)) then
+					hit(plr)
+					highlightPlayer(plr)
+					break
+				end
+			end
+		end
+	end)
+end
+
+function disableFlingSelectMode()
+	if mouseConnection then
+		mouseConnection:Disconnect()
+		mouseConnection = nil
+	end
+	local player = game.Players.LocalPlayer
+	local mouse = player:GetMouse()
+	mouse.Icon = ""
+	removeHighlight()
+end
+
+slap = findslap()
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	local args = string.split(msg, " ")
+	print(args[1],args[2])
+	if args[1] == "/fling" then
+		if args[2] == nil or args[2] == "" then
+			enablePlayerSelection()
+		elseif args[2] == "all" then
+			for _, player in game.Players:GetPlayers() do
+				if player ~= game.Players.LocalPlayer then
+					if player.Character then
+						hit(player)
+					end
+				end
+			end
+		else
+			for _, player in game.Players:GetPlayers() do
+				if player.Name == args[2] then
+					if player.Character then
+						hit(player)
+					end
+				end
+			end
+		end
+	elseif args[1] == "/kill" then
+		if args[2] == "all" then
+			for _, player in game.Players:GetPlayers() do
+				if player ~= game.Players.LocalPlayer then
+					killslap(player)
+				end
+			end
+		else
+			for _, player in game.Players:GetPlayers() do
+				if player.Name == args[2] then
+					killslap(player)
+				end
+			end
+		end
+	elseif args[1] == "/flingselect" then
+		flingSelectMode = not flingSelectMode
+		if flingSelectMode then
+			enableFlingSelectMode()
+			print("Fling select mode enabled")
+		else
+			disableFlingSelectMode()
+			print("Fling select mode disabled")
+		end
+	end
+end)
+
